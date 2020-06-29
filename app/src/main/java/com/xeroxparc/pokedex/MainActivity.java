b7377abd24aadb0a1235246702aa996a6514b706
Fabio Buracchi <45599613+Sph00b@users.noreply.github.com>
2020-06-29 05:47:31 +0200

    Add #1
    - Add navigation ui resource
    - Add activity, layouts and values related to Navigation UI
    - Only affects highlight class

--- app/src/main/java/com/xeroxparc/pokedex/MainActivity.java
+++ app/src/main/java/com/xeroxparc/pokedex/MainActivity.java
@@ -1,14 +1,77 @@
 package com.xeroxparc.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.xeroxparc.pokedex.databinding.ActivityMainBinding;
+
+import java.util.Objects;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Holder holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        holder = new Holder(this);
+        setContentView(holder.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return holder.onNavigateUp() || super.onSupportNavigateUp();
+    }
+
+    class Holder {
+
+        private final ActivityMainBinding binding;
+        private final NavController navController;
+        private final AppBarConfiguration appBarConfiguration;
+
+        Holder(AppCompatActivity activity){
+            binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+            setSupportActionBar(binding.appBar.toolbar);
+
+            navController = ((NavHostFragment) (Objects.requireNonNull(getSupportFragmentManager()
+                    .findFragmentById(binding.contentPanel.navigationHostFragment.getId()))))
+                    .getNavController();
+
+            appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph())
+                    .setOpenableLayout(binding.drawerLayout)
+                    .build();
+
+            NavigationUI.setupActionBarWithNavController(
+                    activity,
+                    navController,
+                    appBarConfiguration
+            );
+            NavigationUI.setupWithNavController(binding.navigationView, navController);
+
+        }
+
+        View getRoot() {
+            return binding.getRoot();
+        }
+
+        Boolean onNavigateUp() {
+            return NavigationUI.navigateUp(navController, appBarConfiguration);
+        }
+
     }
 }
---
+++ app/src/main/java/com/xeroxparc/pokedex/ui/HomeFragment.java
@@ -0,0 +1,31 @@
+package com.xeroxparc.pokedex.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xeroxparc.pokedex.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
