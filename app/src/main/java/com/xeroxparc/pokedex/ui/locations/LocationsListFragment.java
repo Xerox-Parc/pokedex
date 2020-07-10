2bd435e9cda774eb6cf4457e2eca18c4266d5596
DaniloDamico <45599533+DaniloDamico@users.noreply.github.com>
2020-07-10 11:14:21 +0200

    Update LocationsListFragment.java

--- app/src/main/java/com/xeroxparc/pokedex/ui/locations/LocationsListFragment.java
+++ app/src/main/java/com/xeroxparc/pokedex/ui/locations/LocationsListFragment.java
@@ -1,5 +1,4 @@
 package com.xeroxparc.pokedex.ui.locations;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,7 +15,6 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.xeroxparc.pokedex.R;
@@ -25,19 +22,16 @@ import com.xeroxparc.pokedex.data.model.location.Location;
 import com.xeroxparc.pokedex.data.repository.LocationsRepository;
 import com.xeroxparc.pokedex.data.repository.RegionRepository;
 import com.xeroxparc.pokedex.databinding.FragmentLocationsListBinding;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 
 public class LocationsListFragment extends Fragment {
     private static final String TAG = "LocationsListFragment";
     private FragmentLocationsListBinding binding;
     final LinkedList<String> locationsList = new LinkedList<>();
     LocationListListAdapter locationListAdapter;
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -46,32 +40,23 @@ public class LocationsListFragment extends Fragment {
         return binding.getRoot();
     }
 
-
     public void setImageSlider(String regionName, List<SlideModel> slideModels) {
-
         // Pass name of drawable image for get and set maps
         int resIdMap1 = getResources().getIdentifier("image_region_" + regionName + "1", "drawable", getContext().getPackageName());
         slideModels.add(new SlideModel(resIdMap1));
-
         if (!regionName.equals("alola") && !regionName.equals("kalos")) {
-
             // Alola and Kalos Region have only 1 map
             int resIdMap2 = getResources().getIdentifier("image_region_" + regionName + "2", "drawable", getContext().getPackageName());
             slideModels.add(new SlideModel(resIdMap2));
         }
     }
-
-    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         final RegionRepository regionRepository = new RegionRepository(getContext());
-
         TextView textViewRegionName = requireView().findViewById(R.id.text_view_region_name);
         ImageSlider imageSlider = view.findViewById(R.id.image_slider_map);
         List<SlideModel> slideModels = new ArrayList<>();
-
         // Get region ID from "Filter by generation" fragment
         int regionId = LocationsListFragmentArgs.fromBundle(requireArguments()).getRegionId();
-
         // From Region ID make API request to get Region ID to set Title
         regionRepository.getRegion(regionId).observe(getViewLifecycleOwner(), region -> {
             region.
@@ -79,54 +64,37 @@ public class LocationsListFragment extends Fragment {
                         locationListAdapter.setLocationNamesList(retreivedRegion.getLocationList());
                         String formattedRegionName = String.format("%s %s", "Region of", Character.toUpperCase(retreivedRegion.getName().charAt(0)) + retreivedRegion.getName().substring(1));
                         textViewRegionName.setText(formattedRegionName);
-
                         setImageSlider(retreivedRegion.getName(), slideModels);
                         imageSlider.setImageList(slideModels, true);
                     });
         });
     }
-
     class Holder {
 
-
         public Holder(FragmentActivity activity) {
-
             locationListAdapter = new LocationListListAdapter(activity, locationsList, getViewLifecycleOwner());
-
             binding.recycleViewLocationsList.setAdapter(locationListAdapter);
             binding.recycleViewLocationsList.setLayoutManager(new LinearLayoutManager(activity));
-
         }
-
     }
-
     private class LocationListListAdapter extends RecyclerView.Adapter<LocationListListAdapter.ViewHolder> {
-
         private final List<String> locationNamesList;
         private final List<Integer> locationIDList;
-
         private LayoutInflater mInflater;
-
         private final LocationsRepository locationsRepository;
         private LifecycleOwner observationLifeCycle;
 
-
         public LocationListListAdapter(Context context, List<String> moveListDataset, LifecycleOwner lifeCycle) {
             mInflater = LayoutInflater.from(context);
-
             this.locationNamesList = moveListDataset;
             locationsRepository = new LocationsRepository(getContext());
-
             locationIDList = new ArrayList<>();
             observationLifeCycle = lifeCycle;
         }
-
         class ViewHolder extends RecyclerView.ViewHolder {
-
             public final TextView locationItemView;
             public final LinearLayout itemLayout;
             final LocationListListAdapter mAdapter;
-
 
             public ViewHolder(View itemView, LocationListListAdapter adapter) {
                 super(itemView);
@@ -134,9 +102,7 @@ public class LocationsListFragment extends Fragment {
                 itemLayout = itemView.findViewById(R.id.linear_layout_location);
                 this.mAdapter = adapter;
             }
-
         }
-
         @NonNull
         @Override
         public LocationListListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -144,37 +110,27 @@ public class LocationsListFragment extends Fragment {
                     parent, false);
             return new ViewHolder(mItemView, this);
         }
-
         private String capitalize(final String line) {
             return Character.toUpperCase(line.charAt(0)) + line.substring(1);
         }
-
         @Override
         public void onBindViewHolder(@NonNull LocationListListAdapter.ViewHolder holder, int position) {
-
             LocationsListFragmentDirections.ActionNavLocationsListToNavLocationsAreaList action = LocationsListFragmentDirections.actionNavLocationsListToNavLocationsAreaList();
             LocationsListFragmentDirections.ActionNavLocationsListToNavLocationsAreaDetails action1 = LocationsListFragmentDirections.actionNavLocationsListToNavLocationsAreaDetails();
-
             String currentElement = locationNamesList.get(position);
-
             // Capitalize the firs character and remove "-" characters
             currentElement = capitalize(currentElement).replace("-", " ");
-
             action.setLocationIdName(currentElement);
             holder.locationItemView.setText(currentElement);
 
-
             holder.itemView.setOnClickListener(item -> {
                 if (locationIDList.size() > position) {
-
                     locationsRepository.getLocation(locationIDList.get(position)).observe(observationLifeCycle, location -> {
                         location.ifPresent(retrievedLocation -> {
-
                             //Retrieval of Location ID to make IDs string
                             List<String> idsChain = retrievedLocation.getAreaList().stream().map(locationArea -> String.valueOf(locationArea.getId())).collect(Collectors.toList());
-
                             // Concatenate ids separated by the character "," into a string
-                            if (idsChain.isEmpty()) {
+                            if (idsChain.size() > 0) {
                                 String serializedIds = "";
                                 for (int i = 0; i < idsChain.size(); i++) {
                                     serializedIds += idsChain.get(i);
@@ -182,7 +138,6 @@ public class LocationsListFragment extends Fragment {
                                         serializedIds += ",";
                                     }
                                 }
-
                                 if(serializedIds.contains(",")){
                                     // If the "," character is present, multiple location areas are
                                     // associated to the string, so pass the ids to "Location Area List" fragment
@@ -194,41 +149,30 @@ public class LocationsListFragment extends Fragment {
                                     Navigation.findNavController(requireView()).navigate(action1);
                                 }
 
-
                             }else{
                                 // Show toast message when a location that has no other details is clicked
                                 Toast.makeText(getContext(),R.string.locations_not_implemented,Toast.LENGTH_LONG).show();
                             }
-
                         });
                     });
-
                 }
-
             });
-
         }
-
 
         public void setLocationNamesList(List<Location> locations) {
             List<String> locationNames = locations.stream().map(Location::getName).collect(Collectors.toList());
             List<Integer> locationIDs = locations.stream().map(Location::getId).collect(Collectors.toList());
-
             locationNamesList.clear();
             locationNamesList.addAll(locationNames);
-
             locationIDList.clear();
             locationIDList.addAll(locationIDs);
-
             notifyDataSetChanged();
         }
-
         @Override
         public int getItemCount() {
             return locationNamesList.size();
         }
     }
-
 
     @Override
     public void onDestroyView() {
